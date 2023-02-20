d1d959696f51dfaf2911bda6f17050d2597c4c4a
Carles Galan <galan.carles@gmail.com>
2023-02-20 19:09:26 +0100

    Add owner to robotSchema

--- src/database/models/Robot.ts
+++ src/database/models/Robot.ts
@@ -8,6 +8,11 @@ export const robotSchema = new Schema({
     endurance: Number,
     creationDate: String,
   },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    requiered: true,
+  },
 });
 
 export const Robot = model("Robot", robotSchema, "robots");
